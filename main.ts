c085a13fa30fd48c28eec923ca9c508d15e1c940
fcoisaias <65827476+fcoisaias@users.noreply.github.com>
2021-06-08 09:31:12 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        . # # # .
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(5000)
+    basic.clearScreen()
 })
