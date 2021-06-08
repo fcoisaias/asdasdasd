7a42d06b85dc9b754166fc915cddb5d2894630a9
fcoisaias <65827476+fcoisaias@users.noreply.github.com>
2021-06-08 10:58:54 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    basic.showLeds(`
-        # # # # #
-        . # # # .
-        . # # # .
-        . # # # .
-        # # # # #
-        `)
-    basic.pause(5000)
-    basic.clearScreen()
+basic.forever(function () {
+    basic.showIcon(IconNames.Chessboard)
+    if (input.lightLevel() > 128) {
+        led.setBrightness(input.lightLevel())
+    } else if (input.lightLevel() > 50 && input.lightLevel() <= 128) {
+        led.setBrightness(input.lightLevel())
+    } else {
+        led.setBrightness(input.lightLevel())
+    }
 })
